c1cbfb9e3f058f9610d97b19a542e82c4e2842eb
Muhammad Anas <anaskhan321246@gmail.com>
2026-06-08 19:46:13 -0700

    chp 3 Exersise_2

---
+++ Chp3_Exercise2.py
@@ -0,0 +1,6 @@
+name="Anas"
+age=16
+if name.startswith("A") and age > 10:
+    print("You can watch coco move!")
+else:
+    print("sorry You cnnt watch")
---
+++ and_or.py
@@ -0,0 +1,8 @@
+#check two condation at same time
+# and, or
+name='abc'
+age= 19
+if name=='abc' and age==9:
+    print("condation true")
+else:
+    print("condation is false")

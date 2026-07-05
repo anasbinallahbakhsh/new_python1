575299f56a18c0af594e00f5ffffd0a9f0c8a8aa
Muhammad Anas <anaskhan321246@gmail.com>
2026-07-04 17:57:16 -0700

    Exercise13

---
+++ Exercise13.py
@@ -0,0 +1,18 @@
+# def greater(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# num1=int((input("enter a first number:")))
+# num2=int(input("enter a second number"))
+#bigger=greater(num1,num2)
+# print(f"{bigger} is grateer")
+def greater(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+num4=int(input("Enter a third number:"))
+num5=int(input("Enter a fourth  number:"))
+bigger=greater(num4,num4)
+print(f"{bigger} is bigger")

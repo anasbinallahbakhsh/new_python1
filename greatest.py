1b61b7da24ac80dcba0f1fa286715e343908f6c9
Muhammad Anas <anaskhan321246@gmail.com>
2026-07-10 05:02:30 -0700

    KISS - keep it simple and stupid

--- greatest.py
+++ greatest.py
@@ -1,8 +1,8 @@
-def graetest(a,b,c):
+def greatest(a,b,c):
     if a>b and a>c:
          return a
     elif b>a and b>c:
       return b
     else:
         return c
-print(graetest(10,20,30))          
+print(greatest(10,20,30))        

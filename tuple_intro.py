3e0c485a0b5cc8bdf6630354a6fca0e4f23c63f1
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-03 10:57:48 -0700

    tuple intro

---
+++ tuple_intro.py
@@ -0,0 +1,15 @@
+# tuple data strcure
+# tuple can store any data type
+# most imporatant  tuple are immutable , once tuple you  created you canot change
+# data inside tuple
+Example=['one','two','three']
+#no append , no insert, no remmove
+# days=["monday","tuesday"]
+#tuples are faster then lists
+
+
+#method 
+#count , index
+#len funcation                                        
+#slicing
+print(Example[:2])

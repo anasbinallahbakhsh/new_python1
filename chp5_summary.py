f9b9efc0db4561cbb343ad08c79e1b2b4005b80d
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-02 22:08:59 -0700

    chp 5 summary

---
+++ chp5_summary.py
@@ -0,0 +1,39 @@
+#list chp summary
+#liast is data strcture that can hold any type of data
+#create liast
+words=["abc","cde","fgh"]
+#you can store anthing in the list
+mixed=[1,2,3,4,[4,5,6],'seven',9.0, None]
+#print(mixed[0])#1
+#print(mixed[1])#2
+#mixed.append(10)
+# print(mixed)
+# mixed.append([10,20,30])
+# print(mixed)
+# mixed.extend([10,20,30])
+# print(mixed)
+#list orderd collecation of item 
+#add data to our list
+#append method
+#mixed.append(10)
+#print(mixed)
+#join two list
+#mixed.insert(1,"inserted")
+#print(mixed)
+#remove data from list
+#popped=mixed.pop()#re,ove from list 
+#print(popped)
+# print(mixed)
+# popped=mixed.pop(1)#remove item at one posation
+# print(popped)
+# print(mixed)
+# print popped
+#remove
+# mixed.remove("seven") 
+# print(mixed)
+# print(mixed)
+# del mixed[3]
+# print(mixed)
+#loop in list
+for i in mixed:
+ print(i)

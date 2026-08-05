39a498cace1b70d6a1f94ad351ac1b10746c2fbd
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-04 21:24:52 -0700

    practic

---
+++ python_ practic/doct_practic.py
@@ -0,0 +1,12 @@
+my_intro=['Anas',16,['coco','madgaser']]
+print(my_intro)
+
+#list inside list
+user_info1={
+    'name':'anas',
+    'age' : 16,
+    'favorite_movies':['coco'],
+    'favorite_tune':['solitude']
+}
+print(user_info1['age'])
+#print(user_info1['age'])

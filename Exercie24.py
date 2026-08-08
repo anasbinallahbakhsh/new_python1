d069cfa0b9fc42ed46cd27cc2384cd1ca16be671
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-07 20:39:57 -0700

    Exercise24

--- Exercie24.py
+++ Exercie24.py
@@ -1,6 +1,6 @@
 def cube_finder(num):
     dict_cubes={}
-    for i in range(1,num+1):
+    for i in range(1,+1):
         dict_cubes[i]=i**3
     return dict_cubes
 print(cube_finder(10))
---
+++ python_ practic/Exercise_24_practic.py
@@ -0,0 +1,7 @@
+def cube_finder(num):
+    dict_cube={}
+    for i in range(1,+5):
+        dict_cube[i]=i**3
+    return dict_cube
+print(cube_finder(5))
+ 

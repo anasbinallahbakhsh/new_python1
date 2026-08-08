99e78f94401c6c6c043e3c109bb8869bd6c42cd0
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-07 19:48:53 -0700

    more about get method

---
+++ more_about_get.py
@@ -0,0 +1,6 @@
+#more about get , two same keys in dactnoriess
+user={'name':'anas','age':'16','age':'17','age':'19'}
+#print(user.get('names','this key is not present'))
+#print(user.get('name','not found'))
+
+print(user)

e76e18e70093b873993d7bce52295de15a1dd895
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-08 22:59:05 -0700

    Exercise

---
+++ store_data_from_user.py
@@ -0,0 +1,7 @@
+users={
+    "name": input("Enter your name:"),
+    "age": int(input("Enter your age:")),
+    "favorite_tune": input("Enter your favorite tune:"),
+    "favorite_song": input("Enter your favorite song:")
+}
+print(users)

2d724365affc191fabb15a79170a53fb672c69aa
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-09 11:05:42 -0700

    ser intro

---
+++ set_intro.py
@@ -0,0 +1,22 @@
+#set data type
+#unorderd collecation of unique items
+#s={1,2,3}
+
+s={1,1.2,"string"}
+
+#print(s)
+# l=[1,2,3,4,5,6,6,6,7,7,8,8,9,]
+
+#remove duplicite
+#s1=list(set(l))
+# print(s1)
+# s.add(4)
+# print(s)
+# s.remove(4)
+#s.discard(44)
+#print(s)
+# s.clear()
+# print(s)
+# s1=s.copy()
+# print(s1)
+print(s)

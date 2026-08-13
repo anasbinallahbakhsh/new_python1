7bb16a0895d167961bd2001f7adaee901aadf049
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-12 18:25:33 -0700

    args

---
+++ args_intro.py
@@ -0,0 +1,14 @@
+# make flexible functions
+# * operator args
+# *args
+
+def total(*args):
+    return sum(args)
+
+def all_total(*args):
+    total = 0
+    for nums in args:
+        total += nums
+    return total
+
+print(all_total(1, 2, 3, 4, 5, 6))

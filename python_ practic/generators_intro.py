c87225bb1b816b538554a3ef2aaf3e968f034764
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-19 21:16:31 -0700

    GENRATIRS INTRO

---
+++ python_ practic/generators_intro.py
@@ -0,0 +1,25 @@
+#genrators
+#genrators iterators
+#iterator ,iterable
+
+l=[1,2,3]#iterable
+
+# for i in l:
+#     print(i)
+# i=iter(l)
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# next(l)
+
+
+
+
+# for num in map(lambda a:a**2, l):#iterator
+#     print(num)
+
+
+
+
+
